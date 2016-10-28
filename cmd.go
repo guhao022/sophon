0540d05bd061a93de25a0026fba3f875dc130b1e
guhao <378999587@qq.com>
2016-10-28 15:26:28 +0800

    cmd

---
+++ cmd.go
@@ -0,0 +1,25 @@
+package sophon
+
+import (
+	"net/url"
+)
+
+// Command 接口定义了访问需要请求资源的方法
+type Command interface {
+	URL() *url.URL
+	Method() string
+}
+
+// 实现一个基本的command
+type Cmd struct {
+	url *url.URL
+	method string
+}
+
+func (cmd *Cmd) URL() *url.URL  {
+	return cmd.url
+}
+
+func (cmd *Cmd) Method() string {
+	return cmd.method
+}
--- request.go
+++
@@ -1,5 +0,0 @@
-package sophon
-
-type Request struct {
-
-}
